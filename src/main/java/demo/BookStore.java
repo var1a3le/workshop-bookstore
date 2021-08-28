51a7d1237230bb508a9269febd2030123f7c7139
up1 <somkiat.p@gmail.com>
2021-08-28 15:01:00 +0700

    Start workshop

---
+++ src/main/java/demo/BookStore.java
@@ -0,0 +1,13 @@
+package demo;
+
+public class BookStore {
+    public static void main(String[] args) {
+        BookStore bookStore = new BookStore();
+        bookStore.process();
+    }
+
+    private void process() {
+        // TODO
+
+    }
+}
